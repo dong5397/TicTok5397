e169e90fcdb3a6f0ddcb3ebabdae80d15fe990bd
dong5397 <dongwok5397@naver.com>
2024-05-19 14:45:02 +0900

    init

--- src/components/Box/StyledBox2.jsx
+++ src/components/Box/StyledBox2.jsx
@@ -4,7 +4,7 @@ import Dot from "../../../images/Home/Dot.png";
 import Label2 from "../Lable/Label2";
 
 const StyledBox2Container = styled.div`
-  margin-top: 60px;
+  margin-top: 50px;
   max-width: 800px;
   width: 100%;
   display: flex;
